552f02334912fc7bd9c7459571e345fcc3f1cd34
EstradaJuan <jdestrada1@pipeline.sbcc.edu>
2021-02-10 15:23:52 -0800

    CSS changes to HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Link, Route, useHistory} from "react-router-dom
 import CreateRoom from './CreateRoom.js'
 import JoinRoom from './JoinRoom.js'
 
-
+const buttomFormat = "cursor-pointer transition-colors bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
 function CreateButton() {
   let history = useHistory();
 
@@ -13,9 +13,11 @@ function CreateButton() {
   }
 
   return (
-    <button type="button" onClick={handleClick}>
-      Create Room
-    </button>
+    <div style={{paddingTop: 20, paddingBottom:20 }}>
+      <button className="MenuButton" type="button" onClick={handleClick}>
+        Create Room
+      </button>
+    </div>
   )
 }
 
@@ -27,17 +29,25 @@ function JoinButton() {
   }
 
   return (
-    <button type="button" color="primary" onClick={handleClick}>
-      Join Room
-    </button>
+    <div style={{paddingTop: 20, paddingBottom:20 }}>
+      <button className="MenuButton" type="button" color="primary" onClick={handleClick}>
+        Join Room
+      </button>
+    </div>
   )
 }
 
-
+const t = {
+  paddingTop: 20,
+  paddiingRight: 20,
+  paddingLeft: 20,
+  paddingBottom: 20
+}
+//{{height: '50%', width: '300px', margin: 'auto'}}>
 function Home() {
   return (
-    <div>
-      <h2>PICKR</h2>
+    <div className='PageFormat' style={t}> 
+      <h1>PICKR</h1>
       <div>
         <CreateButton/>
       </div>
@@ -47,7 +57,6 @@ function Home() {
     </div>
   );
 }
-
 
 function App() {
   return (
@@ -60,5 +69,6 @@ function App() {
     </Router>
   )
 }
+
 
 export default App;
--- frontend/src/JoinRoom.js
+++ frontend/src/JoinRoom.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom';
 
 function JoinRoom() {
     return (
-        <div>
+        <div className='PageFormat'>
             <h2>Join Room</h2>
             <form>
                 <label>
