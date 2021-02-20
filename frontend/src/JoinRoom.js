3a2e157c88aa88467cd267302bde33f8f71f0128
EstradaJuan <jdestrada1@pipeline.sbcc.edu>
2021-02-19 20:33:08 -0800

    Join room page UI update

--- frontend/src/JoinRoom.js
+++ frontend/src/JoinRoom.js
@@ -2,15 +2,37 @@ import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
 
+function SubmitButton() {
+
+    function handleClick() {
+        console.log("code: ");
+      }
+    return (
+      <div style={{paddingTop: 20, paddingBottom:20 }}>
+        <button className="MenuButton" type="button" >
+          Join
+        </button>
+      </div>
+    )
+  }
+
+let handleSubmit = (event) => {
+    event.preventDefault()
+    let roomCode = event.target[0].value
+    console.log("Code entered: " + roomCode)
+}
 function JoinRoom() {
     return (
         <div className='PageFormat'>
             <h2>Join Room</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Room Code:
-                    <input type="text"/>
                 </label>
+                <br></br>
+                <input class="InputField" type="text" maxLength="5"/>
+                <br></br>
+                <button className="SubmitButton" type="submit"> Submit</button>
             </form>
         </div>
     );
