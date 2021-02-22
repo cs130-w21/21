52b86a4fe735336e98ed82c4b903cd94ab4fbb1d
EstradaJuan <jdestrada1@pipeline.sbcc.edu>
2021-02-21 18:29:56 -0800

    Finished UI for creating a room

--- frontend/src/CreateRoom.js
+++ frontend/src/CreateRoom.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function CreateRoom() {
+function CreateRoomButton() {
+  
+    function handleClick() {
+        if (optionClicked != 0)
+            console.log(optionClicked) //This is where we would want to POST request
+
+    }
+  
     return (
-        <h2>Create Room page</h2>
+      <div style={{paddingTop: 20, paddingBottom:20 }}>
+        <button className="MenuButton" type="button" onClick={handleClick}>
+          Create Room
+        </button>
+      </div>
+    )
+  }
+let optionClicked = 0;
+
+function CreateRoom() {
+    let onSelectChange = (cb) => { optionClicked = cb.currentTarget.value}
+    return (
+        <div className='PageFormat'>
+            <h2>Choose Room Settings</h2>
+            <select className="DropdownMenu" onChange={onSelectChange}>
+                <optgroup >
+                    <option value ="0">Select an Option</option> 
+                    <option value="1">UCLA Study Locations</option>
+                    <option value="2">UCLA Food Options</option>
+                    <option value="3">Custom</option>
+                </optgroup>
+            </select>
+            <CreateRoomButton />
+        </div>
     );
 }
 
